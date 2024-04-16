30580c553c88425e5d3ce45b74b80c3c0c513777
radoslav.titko <radoslav.titko@ringcentral.com>
2024-04-16 13:52:31 +0300

    Some design added

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express()
 app.use(express.json())
-app.use('/', express.static(path.join(__dirname + '/public/templates')))
+app.use('/', express.static(path.join(__dirname + '/public/main_page')))
 
 // const port = 3000
 
---
+++ js/public/main_page/scripts.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+});
+
+function myMenuFunction() {
+  var i = document.getElementById("navMenu");
+
+  if (i.className === "nav-menu") {
+    i.className += " responsive";
+  } else {
+    i.className = "nav-menu";
+  }
+}
+
+function login() {
+  var a = document.getElementById("loginBtn");
+  var b = document.getElementById("registerBtn");
+  var x = document.getElementById("login");
+  var y = document.getElementById("register");
+
+  x.style.left = "4px";
+  y.style.right = "-520px";
+  a.className += " white-btn";
+  b.className = "btn";
+  x.style.opacity = 1;
+  y.style.opacity = 0;
+}
+
+function register() {
+  var a = document.getElementById("loginBtn");
+  var b = document.getElementById("registerBtn");
+  var x = document.getElementById("login");
+  var y = document.getElementById("register");
+
+  x.style.left = "-510px";
+  y.style.right = "5px";
+  a.className = "btn";
+  b.className += " white-btn";
+  x.style.opacity = 0;
+  y.style.opacity = 1;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('idHome').addEventListener('click', function() {
+    fetch('http://localhost:8080/test-endpoint', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: 'Hello from the frontend!' })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => console.log('Response from the backend:', data))
+    .catch(error => console.error('Error:', error));
+  });
+});
+
+
+  
