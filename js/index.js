f1acc25c8721be2b754c6a683e1cec2813e33757
radoslav.titko <radoslav.titko@ringcentral.com>
2024-07-01 17:00:39 +0300

    Lets continue this work yuuu

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express()
 app.use(express.json())
-app.use('/', express.static(path.join(__dirname + '/public/main_page')))
+app.use('/', express.static(path.join(__dirname + '/public/login_page')))
 
 // const port = 3000
 
